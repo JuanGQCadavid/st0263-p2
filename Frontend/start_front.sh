63ca13a3071e6e7635ae00d1ff7e08d167e0073b
JuanGQCadavid <jquiro12@eafit.edu.co>
2019-09-15 20:39:10 -0500

    Adding docker compose to script

--- Frontend/start_front.sh
+++ Frontend/start_front.sh
@@ -1,20 +1,18 @@
 #!bin//bash
 
-
 echo "Checking if docker is installed"
 if ! [ -x "$(command -v docker)" ]; then
-    echo 'Docker is no installed.. Starting installation'
-    wget -qO- https://get.docker.com/ | sh
+
+    echo "Install and start docker"
+    yum update -y
+    yum install -y docker
+    service docker start
+
+    usermod -a -G docker ec2-user
+    echo "Installing docker compose"
     sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
-    sudo usermod -aG docker ubuntu
-    
-    # Auto-start on boot
-    sudo systemctl enable docker
-    
-    #Start rigth now
-    sudo systemctl start docker
-    
+
     
 else
     echo 'Docker is installed'
